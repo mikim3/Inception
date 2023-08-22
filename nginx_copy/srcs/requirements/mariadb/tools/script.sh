c0ca13db8b3696bcc4a7e4dd077aacefcb21f7da
김민수 <minsu980107@naver.com>
2023-08-22 16:02:52 +0900

    mariadb 공식문서 내용 반영

--- nginx_copy/srcs/requirements/mariadb/tools/script.sh
+++ nginx_copy/srcs/requirements/mariadb/tools/script.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
+mkdir -p /docker-entrypoint-initdb.d
+
 # /var/lib/mysql 하위 폴더들에 대한 mysql의 권한부여
 chown -R mysql:mysql /var/lib/mysql
 
-service mariadb start
-sleep 0.1
+# docker-entrypoint-initdb.d/*.sql 은 미리 정해진 경로
+echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > /docker-entrypoint-initdb.d/init.sql
+echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> /docker-entrypoint-initdb.d/init.sql
+echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> /docker-entrypoint-initdb.d/init.sql
+echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/init.sql
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > init.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> init.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-
-mysql < init.sql
-
-service mariadb stop
-sleep 0.1
-
-mariadbd
+# MariaDB를 포그라운드에서 실행
+exec mariadbd
